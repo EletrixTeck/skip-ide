d738a27cc426e18a1aac7267c6b0a879c6e2cafc
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-07-11 21:28:12 +0200

    Update other.py

--- other.py
+++ other.py
@@ -2,9 +2,11 @@ import requests
 import yaml
 import tkinter as tk
 from tkinter import messagebox
+import os
+import webbrowser
 current_version = 0.1
 def check_for_updates():
-    url = 'https://raw.githubusercontent.com/EletrixtimeYT/skip-ide/main/version.yaml'
+    url = 'https://raw.githubusercontent.com/EletrixTeck/skip-ide/main/version.yaml'
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -12,8 +14,9 @@ def check_for_updates():
         if latest_version != current_version:
             message = f"A new version ({latest_version}) of the program is available! Would you like to download it?"
             if messagebox.askokcancel("Update Available", message):
-                import webbrowser
-                webbrowser.open('https://github.com/EletrixtimeYT/skip-ide/releases/latest')
+                 webbrowser.open('https://github.com/EletrixTeck/skip-ide/releases/latest')
     except:
+        
         pass
-syntax = ["test"]
+def changelog():
+     webbrowser.open('changelog.html')
